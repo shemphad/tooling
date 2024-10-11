d70f942c4570ef188214a7932350bae20d53e557
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-10 19:14:47 -0500

    add jenkins script

--- modules/jenkins-server/jenkins.sh
+++ modules/jenkins-server/jenkins.sh
@@ -13,6 +13,10 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
+#Install jenkins
+sudo apt update -y
+sudo apt-get install jenkins -y
+
 # Start Jenkins service
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
